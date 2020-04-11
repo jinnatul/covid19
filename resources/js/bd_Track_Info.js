4ba2186b00e29361d21c2f726746682dc7e60b77
jinnatul <morolswediu@gmail.com>
2020-04-11 18:56:54 +0600

    update data 11-04-2020

--- resources/js/bd_Track_Info.js
+++ resources/js/bd_Track_Info.js
@@ -4,6 +4,7 @@ $(function() {
 
     bd_Info_Array.push({Date: '09-04-2020', T_Tests: 'x',T_Positive_Cases: 112, T_Deaths: 1, T_Recovary: 'x'});
     bd_Info_Array.push({Date: '10-04-2020', T_Tests: 1184,T_Positive_Cases: 94, T_Deaths: 6, T_Recovary: 'x'});
+    bd_Info_Array.push({Date: '11-04-2020', T_Tests: 954,T_Positive_Cases: 58, T_Deaths: 3, T_Recovary: 3});
 
     let bd_table = "<table class='highlight'><tr><th>Date</th><th>Today_Tests</th><th>Today_Positive</th>";
         bd_table += "<th>Today_Deaths</th><th>Today_Recovary</th></tr>"
