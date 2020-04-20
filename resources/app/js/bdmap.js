af6cf438006414b6941101197cf880bc7a628204
jinnatul <morolswediu@gmail.com>
2020-04-20 13:34:27 +0600

    add api on bdmap

--- resources/app/js/bdmap.js
+++ resources/app/js/bdmap.js
@@ -1,44 +1,68 @@
-var eachDistrict = document.querySelector('#each_District');
+$(function() {
+    let apiURL = "https://corona-bd.herokuapp.com/district"
 
-window.addEventListener("load", function() {
+    $.get(apiURL, function() {})
+        .done(function(res) {
+            mapMaker(res);
+        });
+})
 
+
+function mapMaker(res) {
+
+    var tooltip = document.querySelector('#each_District');
     var svgObject = document.getElementById('district_Map').contentDocument;
     var svgSelector = svgObject.querySelector('.country');
 
-    //set color on Each district base on affected count
-    for (let index = 0; index < positiveNumbers.length; index++) {
-        let disName = "#" + districtName[index];
-        if (positiveNumbers[index] > 0 && positiveNumbers[index] <= 20) {
+    let dataMAP = {};
+
+    for (let index = 0; index < res["data"].length; index++) {
+        let name = res["data"][index]["name"];
+        name = name.replace("Dhaka City", "Dhaka")
+        name = name.replace("Munshigonj", "Munshiganj")
+        name = name.replace("Laksmipur", "Lakshmipur")
+        name = name.replace("B. Baria", "Brahmanbaria")
+        name = name.replace("Moulovi Bazar", "Moulvibazar")
+        name = name.replace("Potuakhali", "Patuakhali")
+        name = name.replace("Hobiganj", "Habiganj")
+        name = name.replace("Panchagar", "Panchagarh")
+        name = name.replace("Cox’s bazar", "CoxsBazar")
+        dataMAP[name] = res["data"][index]["count"]
+
+        //set color on Each district base on affected count
+        let disName = "#" + name;
+        if (name === "Dhaka (District)") continue;
+        if (res["data"][index]["count"] > 0 && res["data"][index]["count"] <= 20) {
             $(svgObject.querySelector(disName)).css("fill", "#e57373");
         }
-        else if (positiveNumbers[index] > 20 && positiveNumbers[index] <= 50) {
+        else if (res["data"][index]["count"] > 20 && res["data"][index]["count"] <= 50) {
             $(svgObject.querySelector(disName)).css("fill", "#ef5350");
         }
-        else if (positiveNumbers[index] > 50 && positiveNumbers[index] <= 100) {
+        else if (res["data"][index]["count"] > 50 && res["data"][index]["count"] <= 100) {
             $(svgObject.querySelector(disName)).css("fill", "#f44336");
         }
-        else if (positiveNumbers[index] > 100) {
+        else if (res["data"][index]["count"] > 100) {
             $(svgObject.querySelector(disName)).css("fill", "#d50000");
         }
     }
-
-    $eachDistrict = $('#each_District');
+    
+    $tooltip = $('#each_District');
     $(svgSelector).mouseover(function(event) {
-        var currentLocation = data[event.target.id];
-        $eachDistrict.html(event.target.id);
+        var currentLocation = dataMAP[event.target.id];
+        $tooltip.html(event.target.id);
         if(currentLocation!==undefined){
-            $eachDistrict.html(currentLocation);
+            $tooltip.html(event.target.id + ": "+ currentLocation);
         }
         else {
-            $eachDistrict.html("eachDistrict");
+            $tooltip.html(event.target.id + ": 0");
         }
 
-        eachDistrict.style.display = 'block';
-        $(eachDistrict).css('top', event.pageY + 250);
-        $(eachDistrict).css('left', event.pageX - 60);
+        tooltip.style.display = 'block';
+        $(each_District).css('top', event.pageY + 250);
+        $(each_District).css('left', event.pageX + 30);
 
     });
     $(svgSelector).mouseleave(function(event) {
-        eachDistrict.style.display = 'none';
+        tooltip.style.display = 'none';
     });
-});
+}
